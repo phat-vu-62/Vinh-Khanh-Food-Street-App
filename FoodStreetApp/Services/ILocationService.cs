1f63c133e4cd8e2615a94d58985ce1fa0cefebb8
phat-vu-62 <phatvu131025@gmail.com>
2026-04-22 17:25:31 +0700

    fix heatmap

--- FoodStreetApp/Services/ILocationService.cs
+++ FoodStreetApp/Services/ILocationService.cs
@@ -2,7 +2,7 @@ namespace FoodStreetApp.Services
 {
     public interface ILocationService
     {
-        Task<Location?> GetCurrentLocationAsync();
+        Task<Location?> GetCurrentLocationAsync();  
         Task<bool> StartTrackingAsync();
         Task StopTrackingAsync();
         event EventHandler<Location>? LocationChanged;
